29ebbadbc46556c08ee5fb4590a29c174bfa848d
ethico <shahriar.hasan.1523@gmail.com>
2021-01-22 02:04:49 +0600

    a3

--- assignment03.js
+++ assignment03.js
@@ -1,79 +1,63 @@
+// https://github.com/Shariar-Hasan/assignment-03
+
 
 
 //function for converting kilometer to meter 
-function kilometerToMeter(kilo){
-    if(kilo >= 0)
-    {
-        return kilo*1000;
+function kilometerToMeter(kilo) {
+    if (kilo >= 0) {
+        return kilo * 1000;
     }
-    else 
-    {
+    else {
         return "Warning! wrong input";
     }
 }
-
 
 
 
 // function for calculating budget
-function budgetCalculator(watch, phone, laptop){
+function budgetCalculator(watch, phone, laptop) {
     var watchPrice = 50, phonePrice = 100, laptopPrice = 500;
-    if(watch >= 0 && phone >= 0 && laptop >= 0)
-    {
+    if (watch >= 0 && phone >= 0 && laptop >= 0) {
         return (watch * watchPrice) + (phone * phonePrice) + (laptop * laptopPrice);
     }
-    else{
+    else {
         return "Warning! wrong input";
     }
 }
 
 
 
-
-
 // function for calculating hotel cost
-function hotelCost(day){
-    switch(true){
+function hotelCost(day) {
+    switch (true) {
         case day > 20:
-            return 1800 + (day-20)*50;
+            return 1800 + (day - 20) * 50;
             break;
 
         case day > 10 && day <= 20:
-            return 1000 +(day-10)*80;
+            return 1000 + (day - 10) * 80;
             break;
 
         case day >= 0 && day <= 10:
-            return day*100;
+            return day * 100;
+            break;
 
         default:
             return "Warning! wrong input";
     }
-
 }
 
 
 
+// function for finding the longest names
+function megaFriend(friendNames) {
+    var megaName = friendNames[0].length, megaIndex = 0;
 
-
-// function for finding the longest friend name
-function megaFriend(){
-
+    for (var i = 0; i < friendNames.length; i++) {
+        if (friendNames[i].length > megaName) {
+            megaName = friendNames[i].length;
+            megaIndex = i;
+        }
+    }
+    return friendNames[megaIndex];
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log(hotelCost("12"));
